93c292570ca67263685e5babe3b65edb58f10e66
caspero94 <pedropereira.email@gmail.com>
2024-07-23 15:19:21 +0200

    up

--- src/data_collection/task_manager.py
+++ src/data_collection/task_manager.py
@@ -29,7 +29,7 @@ class TaskManager:
                     logger.info(f"""Collect_data --> {exchange} --> {ticker} --> {
                         timeframe} -> {lastdata}""")
 
-                await asyncio.sleep(60)
+                await asyncio.sleep(70)
         except Exception as e:
             logger.error(f"""Error en collect_data: {
                          e}, ticker {ticker} - {timeframe}""")
