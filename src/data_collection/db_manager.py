86cafbaca005e19dc34efd0144eb4d51c24b4e93
caspero94 <pedropereira.email@gmail.com>
2024-07-25 13:11:24 +0200

    update

--- src/data_collection/db_manager.py
+++ src/data_collection/db_manager.py
@@ -32,8 +32,8 @@ class AbstractDBManager(ABC):
 class DBManager(AbstractDBManager):
     def __init__(self, db_url: str):
         self.engine = create_async_engine(
-            # , pool_size=50, max_overflow=30
             db_url, echo=False, connect_args={"timeout": 120})
+        # , pool_size=50, max_overflow=30
         self.async_session = sessionmaker(
             bind=self.engine,
             expire_on_commit=False,
