00737e7eddf14c926cec5dbfdc6757f0550733ff
caspero94 <pedropereira.email@gmail.com>
2024-07-30 18:54:56 +0200

    add services start

---
+++ start_services.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/pedro_pereira/projectX/env/bin/python /home/pedro_pereira/projectX/src/main.py &
+/home/pedro_pereira/projectX/env/bin/python /home/pedro_pereira/projectX/src/main_api.py &
+wait
